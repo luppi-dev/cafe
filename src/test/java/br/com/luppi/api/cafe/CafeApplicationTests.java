a20cf8ab18bff3243c1d09653dd0a2eb6ba11213
Gabriel Luppi <luppidev.gg@gmail.com>
2024-06-07 22:34:23 -0300

    Initial commit

---
+++ src/test/java/br/com/luppi/api/cafe/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luppi.api.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
