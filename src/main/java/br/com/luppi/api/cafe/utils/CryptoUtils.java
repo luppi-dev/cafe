468397e0e6f03077891dc21c129e041f696b2597
Gabriel Luppi <luppidev.gg@gmail.com>
2024-06-17 01:16:46 -0300

    Added bouncy castle dependencies.
    Utility class for encrypting and verifying passwords

---
+++ src/main/java/br/com/luppi/api/cafe/utils/CryptoUtils.java
@@ -0,0 +1,28 @@
+package br.com.luppi.api.cafe.utils;
+
+import org.bouncycastle.crypto.generators.BCrypt;
+
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public class CryptoUtils {
+
+    private static final int NUM_OF_ITERATIONS = 10;
+
+
+    public static byte[] generateSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt);
+        return salt;
+    }
+
+    public static byte[] hashWithBcrypt(String content, byte[] salt) {
+        byte[] contentBytes = BCrypt.passwordToByteArray(content.toCharArray());
+        return BCrypt.generate(contentBytes, salt, NUM_OF_ITERATIONS);
+    }
+
+    public static boolean verify(byte[] hashToVerify, String content, byte[] salt) {
+        return Arrays.equals(hashToVerify, hashWithBcrypt(content,salt));
+    }
+}
---
+++ src/test/java/br/com/luppi/api/cafe/utils/CryptoUtilsTest.java
@@ -0,0 +1,33 @@
+package br.com.luppi.api.cafe.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CryptoUtilsTest {
+
+    private static final String PASSWORD_1 = "123";
+    private static final byte[] SALT_1 = CryptoUtils.generateSalt();
+    private static final String PASSWORD_2 = "321";
+    private static final byte[] SALT_2 = CryptoUtils.generateSalt();
+
+    @Test
+    public void givenSamePasswordAndSalt_thenHashIsCorrect() {
+        byte[] hash = CryptoUtils.hashWithBcrypt(PASSWORD_1, SALT_1);
+        Assertions.assertTrue(CryptoUtils.verify(hash, PASSWORD_1, SALT_1));
+    }
+
+    @Test
+    public void givenDifferentPassword_thenHashIsIncorrect() {
+        byte[] hash = CryptoUtils.hashWithBcrypt(PASSWORD_1, SALT_1);
+        Assertions.assertFalse(CryptoUtils.verify(hash, PASSWORD_2, SALT_1));
+    }
+
+    @Test
+    public void givenDifferentSalt_thenHashIsIncorrect() {
+       byte[] hash = CryptoUtils.hashWithBcrypt(PASSWORD_1, SALT_1);
+       Assertions.assertFalse(CryptoUtils.verify(hash, PASSWORD_1, SALT_2));
+    }
+}
