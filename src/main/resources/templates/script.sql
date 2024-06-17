ad15366d5f17d4ab38d2b1f3c06bf0970ecc4444
Gabriel Luppi <luppidev.gg@gmail.com>
2024-06-16 22:53:24 -0300

    Removed NOT NULL constraint for descriptions, add table income_distribution_ and set default value NOW() for created_at cols

--- src/main/resources/templates/script.sql
+++ src/main/resources/templates/script.sql
@@ -4,11 +4,9 @@ CREATE TABLE user_ (
 	name VARCHAR NOT NULL,
 	email VARCHAR UNIQUE NOT NULL,
 	hashed_password VARCHAR NOT NULL,
-	salt_hash BYTEA NOT NULL
+	salt_hash BYTEA NOT NULL,
+	created_at DATE DEFAULT NOW()
 );
-
-ALTER TABLE user_
-ADD COLUMN created_at date NOT NULL;
 
 CREATE TYPE financial_category AS ENUM (
 	'trip',
@@ -28,10 +26,21 @@ CREATE TABLE financial_goal_ (
 	exposed_id UUID NOT NULL,
 	user_id INTEGER REFERENCES user_(id),
 	name varchar NOT NULL,
-	description VARCHAR NOT NULL,
+	description VARCHAR,
 	category financial_category NOT NULL,
 	target_amount numeric(12, 2) NOT NULL,
 	current_amount numeric(12, 2) DEFAULT 0,
 	target_date date,
-	created_at date NOT NULL
+	created_at DATE DEFAULT NOW()
+);
+
+CREATE TABLE income_distribution_ (
+    id SERIAL PRIMARY KEY,
+    exposed_id UUID NOT NULL,
+    user_id INTEGER REFERENCES user_(id),
+    name VARCHAR NOT NULL,
+    description VARCHAR,
+    category financial_category NOT NULL,
+    percentage numeric(5, 2) NOT NULL,
+    created_at DATE DEFAULT NOW()
 );
